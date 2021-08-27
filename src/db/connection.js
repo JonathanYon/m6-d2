9cf18be82f9cbd7ea9ae3df8a65f8cbc0b8987fc
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-27 20:42:34 +0200

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,19 +2,21 @@ import pg from "pg";
 const { Pool } = pg;
 // pools will use environment variables
 // for connection information
-console.log(process.env.NODE_ENV);
-const db = new Pool({
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
-  // connectionString:
-  //   process.env.NODE_ENV !== "development"
-  //     ? process.env.DATABASE_URL
-  //     : process.env.DATABASE_URL_DEV,
-});
+// console.log("====>", process.env.NODE_ENV);
+// const db = new Pool({
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+//   connectionString:
+//     process.env.NODE_ENV !== "development"
+//       ? process.env.DATABASE_URL
+//       : process.env.DATABASE_URL_DEV,
+// });
 // pool.query('SELECT NOW()', (err, res) => {
 //   console.log(err, res)
 //   pool.end()
 // })
+
+const db = new Pool();
 
 export default db;
