5a9a42ec900ee92083beb4e13f29dd484ec3897f
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-12-07 13:02:50 +0100

    pool

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,44 +1,57 @@
-import pg from "pg";
-const { Pool } = pg;
-// pools will use environment variables
-// for connection information
-console.log("====>p/d", process.env.NODE_ENV);
-const db = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "god is all",
-  port: "5432",
-});
+// import pg from "pg";
+// const { Pool } = pg;
+// // pools will use environment variables
+// // for connection information
+// console.log("====>p/d", process.env.NODE_ENV);
+// // const db = new Pool({
+// //   user: "postgres",
+// //   host: "localhost",
+// //   database: "postgres",
+// //   password: "god is all",
+// //   port: "5432",
+// // });
 
-// const db = new Pool(
-//   {
+// const db = new Pool({
 //   ssl: {
 //     rejectUnauthorized: false,
 //   },
 //   connectionString:
-//     process.env.NODE_ENV !== "development"
+//     process.env.NODE_ENV === "production"
 //       ? process.env.DATABASE_URL
 //       : process.env.DATABASE_URL_DEV,
-// }
-// )
-// db.query("SELECT NOW()", (err, res) => {
-//   console.log("err", err, res);
-//   db.end();
 // });
+// // db.query("SELECT NOW()", (err, res) => {
+// //   console.log("err", err, res);
+// //   db.end();
+// // });
 
-// const { NODE_ENV, DATABASE_URL } = process.env;
+// // const { NODE_ENV, DATABASE_URL } = process.env;
 
-// const isDeployed = NODE_ENV === "production";
-// // conditional ssl config
+// // const isDeployed = NODE_ENV === "production";
+// // // conditional ssl config
 
-// const sslConfig = isDeployed ? { ssl: { rejectUnauthorized: false } } : {};
+// // const sslConfig = isDeployed ? { ssl: { rejectUnauthorized: false } } : {};
 
-// // then spread it into Pool
+// // // then spread it into Pool
 
-// const db = new Pool({
-//   ...sslConfig, //  spreading sslConfig conditionally
-//   connectionString: DATABASE_URL,
-// });
+// // const db = new Pool({
+// //   ...sslConfig, //  spreading sslConfig conditionally
+// //   connectionString: DATABASE_URL,
+// // });
+
+// export default db;
+import pg from "pg";
+
+const { Pool } = pg;
+
+const db = new Pool({
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionString:
+    process.env.NODE_ENV !== "development"
+      ? process.env.DATABASE_URL
+      : process.env.DATABASE_URL_DEV,
+});
 
 export default db;
